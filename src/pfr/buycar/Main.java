a3db72a93d65c1ae275e9f37d1d5fb597003d952
Nicolas <thecyn3tik@gmail.com>
2025-10-14 20:26:56 +0200

    Ajout logique d'ajout utilisateur en base + main pour test

--- src/pfr/buycar/Main.java
+++ src/pfr/buycar/Main.java
@@ -1,9 +1,37 @@
 package pfr.buycar;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import pfr.buycar.dao.DBConnection;
+import pfr.buycar.dao.UtilisateurDao;
+import pfr.buycar.dao.clientMock;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws SQLException {
+		
+		System.out.println("===Test de la connexion à la base de données===");
+		
+		Connection conn = DBConnection.getConnection();
+		
+		if (conn != null) {
+            System.out.println("Connexion active : " + conn);
+        } else {
+            System.err.println("Échec de la connexion à la base !");
+        }
+		
+		System.out.println("\n=== Tests DAO ===");
+		
+		UtilisateurDao userTest = new UtilisateurDao(conn);
+		
+		userTest.create(new clientMock("Dupont", "Serge", "sdupont", "12345", 2, "serge@exemple.com"));
+		
+			
+		// Methode getAll à définir dans l'interface CRUD
+		//System.out.println("Utilisateurs : " + userTest.getAll());
+		
+		 DBConnection.closeConnection();
 
 	}
 
