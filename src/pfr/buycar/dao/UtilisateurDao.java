97fb49afd50ad6ce3642ccc215a31ab5d2db67b8
seynaboumbayeinfo <seynabou.mbaye.info@gmail.com>
2025-10-15 10:50:45 +0200

    initialisation

--- src/pfr/buycar/dao/UtilisateurDao.java
+++ src/pfr/buycar/dao/UtilisateurDao.java
@@ -5,10 +5,6 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
-
-
-
-
 public class UtilisateurDao implements CRUDable<UtilisateurMock>{
 	
 	//private static final List<UtilisateurMock> utilisateurs = new ArrayList<>();
